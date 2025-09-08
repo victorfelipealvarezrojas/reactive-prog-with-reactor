4372470bc1bf9828becb022edd88e7cbb4cd75ee
victor alvarez rojas <victoralvarezrojas@Mac-Studio.local>
2025-09-07 23:07:23 -0300

    update

--- src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,21 +1,28 @@
 package com.learnreactiveprogramming.service;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public class FluxAndMonoGeneratorService {
 
     public Flux<String> namesFlux() {
-        return Flux.fromIterable(List.of("victor", "felipe", "joko", "peras"));
+        return Flux.fromIterable(List.of("victor", "felipe", "joko", "peras"))
+                .log();
     }
+    public Mono<String> namesMonono() {
+        return Mono.just("victor").log();
+    }
+
 
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         fluxAndMonoGeneratorService.namesFlux()
-                .subscribe(name -> {
-                    System.out.println(name);
-                });
+                .subscribe(System.out::println);
+
+        fluxAndMonoGeneratorService.namesMonono()
+                .subscribe(System.out::println);
 
     }
 }
